739a138a75372a3e29f8729c6eb17322560f0fac
mason-macdougall <47550026+mason-macdougall@users.noreply.github.com>
2022-04-24 22:49:07 -0700

    MVP web-app to test algo

---
+++ linker_app.py
@@ -0,0 +1,125 @@
+import pdfkit
+from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader
+from datetime import date
+import streamlit as st
+from streamlit.components.v1 import iframe
+import numpy as np
+import pandas as pd
+import json
+from scipy import stats
+from utils import *
+
+limit = 20
+
+st.set_page_config(layout="centered", page_icon="🔗", page_title="Linker UCLA")
+st.title("Linker UCLA")
+
+st.write(
+    "Matching people based on personality!"
+)
+
+options = ["strongly disagree", "somewhat disagree", "neutral", "somewhat agree", "strongly agree"]
+
+N_options = len(options)
+options_dict = {}
+for i, opt in enumerate(options):
+    options_dict[opt] = i+1
+
+form = st.form(key="annotation")
+
+genders_list = ["", "female", "male", "non-binary", "gender-fluid", "transgender", "other"]
+preference_list = ["anyone", "female", "male", "non-binary", "other"]
+pronouns_list = ["", "she/her", "he/him", "they/them", "he/they", "she/they", "xie/xir", "ze/zir", "other"]
+bio_default = "Let\'s link!"
+
+with form:
+    cols1 = st.columns((1, 1))
+    first = cols1[0].text_input("First:")
+    last = cols1[1].text_input("Last:")
+
+    cols2 = st.columns((1, 1))
+    age = cols2[0].text_input("Age:")
+    pronouns = cols2[1].selectbox(
+        "Preferred pronouns:",
+        pronouns_list,
+        index=0,
+    )
+
+    cols3 = st.columns((1, 1))
+    gender = cols3[0].selectbox(
+        "Preferred gender:",
+        genders_list,
+        index=0,
+    )
+    preference = cols3[1].selectbox(
+        "Preferred gender to link with (if any):",
+        preference_list,
+        index=0,
+    )
+
+    bio = st.text_input("Bio:", value=bio_default, max_chars=50)
+    questions_df = pd.read_csv('questions.csv', delimiter='%')
+    questions = questions_df.Questions.values
+
+    answers = []
+    skips = []
+    for i, q in enumerate(questions):
+        cols = st.columns(2)
+        answers.append(cols[0].select_slider("Q"+str(i+1)+".  "+q, options=options, value="neutral"))
+        skips.append(cols[1].checkbox("Skip Q"+str(i+1)+"?", value=True))
+
+    submitted = st.form_submit_button(label="Get linked!")
+
+if submitted: 
+
+    with open('users.json') as json_file:
+        users = json.load(json_file)
+    json_file.close()
+
+    answers_final = []
+    for i, ans in enumerate(answers):
+        if skips[i] == True:
+            answers_final.append(np.nan)
+        else:
+            answers_final.append(options_dict[ans])
+
+    current_user = {'user_0': {'name': {'first': first, 'last': last},
+                  'age': age,
+                  'gender': gender,
+                  'pronouns': pronouns,
+                  'preference': preference,
+                  'public': True,
+                  'bio': bio,
+                  'answers': np.array(answers_final)}}
+
+    if gender == 'male' and preference == 'female':
+        preference = 'anyone'
+
+    users = get_filtered(users, preference)
+    top_final_scores, argsort_idx = get_top_scores(current_user, users, limit, N_options=N_options)
+    top_profiles = get_top_profiles(users, argsort_idx, limit)
+    profile_list = get_profile_list(top_profiles, top_final_scores)
+
+    df = pd.DataFrame(profile_list).T
+    df_display = df.reset_index().drop('index',axis=1).copy()
+
+    st.subheader("Here are your strongest links 👇 ")
+    st.text("")
+
+    st.table(df_display)
+
+
+
+
+st.write("")
+st.write("")
+st.write("")
+st.write("")
+st.write(
+    r"DISCLAIMER: This is for reference and estimation purposes only. We\'re pretty sure the calculations are right, but you never know ¯\\\_(ツ)\_/¯"
+)
+st.write("")
+st.write("")
+st.write(
+    "To report a bug, email jobcompcalc@gmail.com with a description of the issue and we\'ll be sure to look into it!"
+)
---
+++ utils.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+
+def get_raw_scores(current_user, logged_users, N_options=5):
+    '''
+    Args:
+        current_user (dict): dictionary with current user ID as the key (str) and 
+            current user answers to the questions as an array of values
+        logged_users (dict): dictionary with all logged user IDs as keys (str) and
+            all logged user answers as an array of values
+    Returns:
+        raw_scores (array): array of raw scores between current user and all logged
+            users; sum of differences between each user with current user across 
+            all questions
+    '''
+    users_matrix = np.array(list(logged_users.values()))
+    current_user_arr = np.array(list(current_user.values()))
+    raw_scores = np.nansum(np.abs(users_matrix - current_user_arr),axis=1)
+    
+    N_qs = np.count_nonzero(~np.isnan(current_user_arr))
+    max_score = N_qs*N_options
+    uncertainty = np.sqrt(N_qs*N_options)
+    
+    return raw_scores, max_score, uncertainty
+
+def get_filtered(users, preference):
+    users_df = pd.DataFrame(users).T
+    
+    preference_dict = {'female': ['female'],
+                       'male': ['male'],
+                       'non-binary': ['non-binary'],
+                       'anyone': ["", "female", "male", "non-binary", "gender-fluid", "transgender", "other"],
+                       'other': ["", "gender-fluid", "transgender", "other"]
+                      }
+    
+    users_df_filtered = users_df[users_df['gender'].isin(preference_dict[preference])].T
+    return users_df_filtered.to_dict()
+
+def get_pop_scores(raw_scores, uncertainty, limit):
+    pop_scores_CI = [[100-stats.percentileofscore(raw_scores, raw_score+uncertainty), 100-stats.percentileofscore(raw_scores, raw_score), 100-stats.percentileofscore(raw_scores, raw_score-uncertainty)] for raw_score in raw_scores[:limit]]
+    return pop_scores_CI
+
+def get_prox_scores(raw_scores, max_score, limit):
+    raw_scores = raw_scores[:limit]
+    prox_scores = (max_score-raw_scores)/max_score*100
+    return prox_scores
+
+def get_answers(users):
+    answers = {user_id: users[user_id]['answers'] for user_id in users.keys()}
+    return answers
+
+def get_top_scores(current_user, logged_users, limit, N_options=5):
+    current, logged = get_answers(current_user), get_answers(logged_users)
+    raw_scores_all, max_score, uncertainty = get_raw_scores(current, logged, N_options=N_options)
+
+    argsort_idx = raw_scores_all.argsort()
+    raw_scores_sorted = np.array(raw_scores_all)[argsort_idx]
+
+    top_pop_scores_CI = get_pop_scores(raw_scores_sorted, uncertainty, limit=limit)
+    top_prox_scores = get_prox_scores(raw_scores_sorted, max_score, limit=limit)
+
+    top_final_scores = [(np.array(pop)+prox)/2.0 for pop, prox in zip(top_pop_scores_CI, top_prox_scores)]
+    
+    return top_final_scores, argsort_idx
+
+def get_top_profiles(logged_users, argsort_idx, limit):
+    top_user_ids = np.array(list(logged_users.keys()))[argsort_idx][:limit]
+    top_profiles = {user_id: logged_users[user_id] for user_id in top_user_ids}
+    
+    return top_profiles
+
+
+def get_profile_list(top_profiles, top_final_scores):
+    profile_list = {}
+    for user_id, score in zip(top_profiles.keys(), top_final_scores):
+        profile_brief = {
+            'name': top_profiles[user_id]['name']['first'],
+            'age': top_profiles[user_id]['age'],
+            'gender': top_profiles[user_id]['gender'],
+            'pronouns': top_profiles[user_id]['pronouns'],
+            'bio': top_profiles[user_id]['bio'],
+            'score': score[1],
+            'score_lower_est': score[0],
+            'score_upper_est': score[2]
+        }
+        profile_list[user_id] = profile_brief
+    return profile_list
+
+
+
+
+
